ae251129c3f16738fc86517273012b87997f8aba
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2023-07-31 09:12:27 +0700

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set;}
+}
---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
